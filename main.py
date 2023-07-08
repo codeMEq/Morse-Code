64a6a6095609a7a89d3de5813218ff4336a1f6e2
Richa <102714979+codeMEq@users.noreply.github.com>
2023-07-08 14:22:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'
+}
+
+def convert_to_morse_code(text):
+    morse = []
+    for char in text:
+        if char.upper() in morse_code:
+            morse.append(morse_code[char.upper()])
+    return ' '.join(morse)
+
+def main():
+    text = input("Enter a string: ")
+    morse = convert_to_morse_code(text)
+    print("Morse code:", morse)
+
+if __name__ == '__main__':
+    main()
